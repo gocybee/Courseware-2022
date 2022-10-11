e574e09dc55624947d6af0488d98a92722ef7b2f
tanice_white <2505772098@qq.com>
2022-10-11 19:41:04 +0800

    finish Lv2

--- class6/example/Lv2/cmd/main.go
+++ class6/example/Lv2/cmd/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	_ "Lv2/dao"
+	"Lv2/global"
+	"fmt"
+)
 
+func main() {
+	var user = global.User{
+		Name:     "TR",
+		Account:  "123456",
+		Password: "654321",
+		Question: "Who are you?",
+		Answer:   "啊吧啊吧啊吧",
+	}
+
+	//增--创建信息
+	if err := global.DB.Model(global.User{}).Create(&user).Error; err != nil {
+		fmt.Println("[WARNING] an error occurred:", err)
+	}
+	MsgPrint()
+
+	//改--更改信息
+	if err := global.DB.Model(global.User{}).Update("password", "10086").Error; err != nil {
+		fmt.Println("[WARNING] an error occurred:", err)
+	}
+	MsgPrint()
+
+	//查--检索信息
+	var t global.User
+	if err := global.DB.Model(global.User{}).Where("password=?", "10086").Find(&t).Error; err != nil {
+		fmt.Println("[WARNING] an error occurred:", err)
+	}
+	MsgPrint()
+
+	//删-删除信息
+	if err := global.DB.Model(global.User{}).Delete(&global.User{}).Error; err != nil {
+		fmt.Println("[WARNING] an error occurred:", err)
+	}
+	MsgPrint()
+}
+
+func MsgPrint() {
+	var t global.User
+	if err := global.DB.Model(global.User{}).First(&t).Error; err != nil {
+		fmt.Println("[WARNING] an error occurred:", err)
+		return
+	}
+	fmt.Println(t)
 }
--- class6/example/Lv2/dao/mysql.go
+++ class6/example/Lv2/dao/mysql.go
@@ -2,24 +2,26 @@ package dao
 
 import (
 	"Lv2/global"
-	"fmt"
+
+	_ "github.com/go-sql-driver/mysql" //优先加载数据库驱动的init函数
+
 	"github.com/jinzhu/gorm"
 )
 
 // init 链接数据库并进行相关的初始化
 func init() {
 	// 链接数据库
-	dsn := "user:@tcp(127.0.0.1:3306)/qff_test?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:030831@tcp(127.0.0.1:3306)/qff_test?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
-		fmt.Println("[WARNING] an error occurred:", err)
+		panic("[WARNING] an error occurred:" + err.Error())
 	}
 
 	// 判断表格是否存在，否 则创建一个表格
 	if !db.HasTable(&global.User{}) {
 		err = db.CreateTable(&global.User{}).Error
 		if err != nil {
-			fmt.Println("[WARNING] an error occurred:", err)
+			panic("[WARNING] an error occurred:" + err.Error())
 		}
 	}
 
