c02c934c09487f7a1e37039250ca4d192acbe416
iGxnon <1545766400@qq.com>
2022-10-29 14:33:55 +0800

    class3: update class3 homework

--- class3/example/01-repeater/main.go
+++ class3/example/01-repeater/main.go
@@ -2,16 +2,57 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var (
+	lock1 = &sync.Mutex{}
+	lock2 = &sync.Mutex{}
+)
+
 func main() {
-	repeater()
+	go repeater1()
+	go repeater1()
+	go repeater1()
+	go repeater2()
+	go repeater1()
+	go repeater1()
+	go repeater1()
+	go repeater2()
+	go repeater2()
+	go repeater2()
+	repeater1()
 }
 
-func repeater() {
+func repeater1() {
 	for {
 		time.Sleep(time.Second)
-		fmt.Println("over.")
+		// fmt.Println("over.")
+		lock1.Lock()
+		lock2.Lock()
+		fmt.Print("o")
+		fmt.Print("v")
+		fmt.Print("e")
+		fmt.Print("r")
+		fmt.Println(".")
+		lock1.Unlock()
+		lock2.Unlock()
+	}
+}
+
+func repeater2() {
+	for {
+		time.Sleep(time.Second)
+		// fmt.Println("over.")
+		lock1.Lock()
+		lock2.Lock()
+		fmt.Print("o")
+		fmt.Print("v")
+		fmt.Print("e")
+		fmt.Print("r")
+		fmt.Println(".")
+		lock2.Unlock()
+		lock1.Unlock()
 	}
 }
