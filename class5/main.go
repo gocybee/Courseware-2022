e1ab430a443900d804bbc4ccde75d40a4122efca
L2ncE <llance_24@foxmail.com>
2022-10-30 22:14:28 +0800

    [WIP]class5: add some courseware

---
+++ class5/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping")
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
