a11fd2d3de69a68bd4261013f11e3f2bfef62ae3
lluckyboi <1598273095@qq.com>
2022-12-01 14:20:51 +0800

    add class 9

---
+++ class9/example/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(static.Serve("/", static.LocalFile("/geekbee", false)))
+	r.Run(":8077")
+}
